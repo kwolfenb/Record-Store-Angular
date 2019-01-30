8c62d631e726738e01917e8ad9eef394e80babce
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-01-29 16:16:03 -0800

    updates

---
+++ src/app/album.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Album } from './album.model';
+import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+
+
+@Injectable()
+export class AlbumService {
+
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) { 
+    this.albums = database.list('albums');
+  }
+
+
+  getAlbums() {
+    return this.albums;
+  }
+
+  getAlbumById(albumId: number) {
+    for (let i=0; i < ALBUMS.length; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
+  }
+
+}
